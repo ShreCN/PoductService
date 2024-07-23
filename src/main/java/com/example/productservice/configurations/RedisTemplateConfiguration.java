b777edbdb23620af136ee4d917b9a101968f1818
Shreyas <shre74024@gmail.com>
2024-07-23 22:52:10 +0530

    Implementing eureka service discovery. Calling userservice in loadbalanced way via @LoadBalanced

---
+++ src/main/java/com/example/productservice/configurations/RedisTemplateConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.productservice.configurations;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RedisTemplateConfiguration {
+    @Bean
+    public RedisTemplate<String, Object> getRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
